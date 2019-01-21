a4ad045e297e73171687cac5f84d3ae38bd7002e
aamir <wasi3aamir@gmail.com>
2019-01-21 17:26:33 +0600

    Accepted programming-feature

---
+++ Programming-Language-Feature/Programming-Language-Feature/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Language_Feature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "C++", "Java", "C#", "PHP", "Python", "Go", "Ruby", "VB.Net", "Objective-C", "JavaScript", "Perl" };
+            
+            Console.WriteLine(name.Length);
+        }
+    }
+}
